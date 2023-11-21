1876ed4b48693100661bc4d23ad5196e355a3b8a
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-11-20 22:35:04 -0500

    new file:   .vscode/settings.json 	modified:   android/app/build.gradle 	new file:   android/app/google-services.json 	modified:   android/build.gradle 	modified:   ios/Flutter/Debug.xcconfig 	modified:   ios/Flutter/Release.xcconfig 	new file:   ios/Podfile 	modified:   ios/Runner.xcodeproj/project.pbxproj 	new file:   ios/Runner/GoogleService-Info.plist 	new file:   ios/firebase_app_id_file.json 	new file:   lib/firebase_options.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/Flutter-Debug.xcconfig 	modified:   macos/Flutter/Flutter-Release.xcconfig 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	new file:   macos/Podfile 	modified:   macos/Runner.xcodeproj/project.pbxproj 	new file:   macos/Runner/GoogleService-Info.plist 	new file:   macos/firebase_app_id_file.json 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pictoria/screen_layout.dart';
 import 'package:pictoria/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
