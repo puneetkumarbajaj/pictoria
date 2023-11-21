f2ee426742e34cb9bd260ab1f467f41652c1b4df
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-11-21 00:39:37 -0500

    modified:   lib/screen_layout.dart 	new file:   lib/screens/login_screen.dart 	new file:   lib/screens/signup_screen.dart 	new file:   lib/widgets/text_field_input.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	new file:   public/Pacifico-Regular.ttf 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screen_layout.dart
+++ lib/screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pictoria/screens/login_screen.dart';
 
 class ScreenLayout extends StatelessWidget {
   const ScreenLayout({super.key});
@@ -6,7 +7,7 @@ class ScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Test")),
+      body: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pictoria/utils/colors.dart';
+import 'package:pictoria/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              //logo
+              Text("Pictoria",
+                  style: GoogleFonts.pacifico(
+                    textStyle: TextStyle(fontSize: 70, color: Colors.white),
+                  )),
+
+              //email
+              const SizedBox(
+                height: 64,
+              ),
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: 'Enter your email',
+                  textInputType: TextInputType.emailAddress),
+
+              //pass
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+
+              //login button
+              InkWell(
+                child: Container(
+                  child: const Text("Log in"),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4))),
+                      color: blueColor,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12,),
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+              
+              //Sign up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text("Not a member already?"),
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text("Sign up", style: TextStyle(fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
